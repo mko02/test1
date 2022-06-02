1a5fa783e05887f32262be5b73885cd36cafc125
Max Ko <maxko@Maxs-MacBook-Air-3.local>
2022-06-02 13:30:31 +0800

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import './App.css';
+//deploy to firebase
+// npm run build
+// firebase deploy
 
+//push to github
+// git add .
+// git commit -m "Add Some Comment"
+// git push -u origin main
+
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+/*
 const schedule = {
   "title": "CSS Courses for 2018-2019",
   "courses": {
@@ -25,15 +35,43 @@ const schedule = {
       "title" : "Tech & Human Interaction"
     }
   }
-};
+}; */
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-  { Object.values(courses).map(course => <Course course={ course } />) }
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+  
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm}/>
+      <div className="course-list">
+      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      </div>
+    </>
+  );
+};
+
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" autoComplete="off"
+      checked={checked} onChange={() => setTerm(term)}/>
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
+);
+
+const TermSelector = ({term, setTerm}) => (
+  <div className="btn-group">
+  { 
+    Object.values(terms).map(value => (
+      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+    ))
+  }
   </div>
 );
 
@@ -56,11 +94,29 @@ const Course = ({ course }) => (
   </div>
 );
 
-const App = () =>  (
+const App = () =>  {
+  const [schedule, setSchedule] = useState();
+
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
+  if (!schedule) return <h1>Loading schedule...</h1>;
+
+  return (
   <div className="container">
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
   </div>
-);
+  )
+};
 
 export default App;
